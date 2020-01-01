c3d2926865951753219cdd582ae2fa563c504599
kendell <KTibow@users.noreply.github.com>
2019-12-31 16:50:49 -0800

    Update test_ffsend.py

--- test_ffsend.py
+++ test_ffsend.py
@@ -12,6 +12,7 @@ import pytest
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
     if report.when == 'call':
