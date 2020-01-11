381f38b97a8e626f41bcc72cb0fcc10323fa3857
Robert Xiao <brx@cs.ubc.ca>
2020-01-10 18:26:00 -0800

    Bump version to 0.1.3 so we can add new instructions to PyPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name="ffsend",
-    version="0.1.2",
+    version="0.1.3",
     description="A Firefox Send client.",
     long_description=long_description,
     long_description_content_type='text/markdown',
