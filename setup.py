5fe46fc0c92eb64ef1033f18af75fc4ff2e62958
Robert Xiao <brx@cs.ubc.ca>
2020-01-10 16:00:13 -0800

    Add classifiers and MPL2.0 license file

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
-import setuptools
+from setuptools import setup
 
 
-DISTRIBUTION_NAME = "ffsend"
-VERSION = "0.1.0"
-DESCRIPTION = "A Firefox Send client."
-STANDALONE_MODULES = ["ffsend"]
-CONSOLE_SCRIPTS = ["ffsend = ffsend:main"]
-INSTALL_REQUIRES = ["clint", "pycryptodome", "requests"]
+with open('README.md') as f:
+    long_description = f.read()
 
-setuptools.setup(
-    name=DISTRIBUTION_NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    py_modules=STANDALONE_MODULES,
-    install_requires=INSTALL_REQUIRES,
-    entry_points={"console_scripts": CONSOLE_SCRIPTS},
+setup(
+    name="ffsend",
+    version="0.1.2",
+    description="A Firefox Send client.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    author = 'Robert Xiao',
+    author_email = 'robert.bo.xiao@gmail.com',
+    url = 'https://github.com/nneonneo/ffsend',
+    license = 'MPL 2.0',
+    classifiers = [
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities",
+        "Topic :: Communications :: File Sharing",
+    ],
+
+    py_modules=["ffsend"],
+    install_requires=["clint", "pycryptodome", "requests"],
+    entry_points={"console_scripts": ["ffsend = ffsend:main"]},
 )
